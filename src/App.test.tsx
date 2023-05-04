7aedd69a357c30cba4fece1d85ad222759880e88
mijst <lukederijk@gmail.com>
2023-05-04 13:03:40 +0100

    added document checker & unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,105 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {App,FilterSearch,FilterData,Folder, filterOptions} from './App';
+import { jsonData } from './JSONData';
 
-test('renders learn react link', () => {
+
+test('Should render component with title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Documents/i);
+  expect(title).toBeInTheDocument();
 });
+
+test('Should render the search component', () => {
+  render(<FilterSearch />);
+  const filter = screen.getByPlaceholderText(/Search.../i)
+  expect(filter).toBeInTheDocument();
+});
+
+test('Should render the sort options', () => {
+  render(<FilterSearch />);
+  expect(screen.getByText(/Sort/)).toBeInTheDocument();
+  expect(screen.getByText(/name/)).toBeInTheDocument();
+  expect(screen.getByText(/type/)).toBeInTheDocument();
+  expect(screen.getByText(/date/)).toBeInTheDocument();
+});
+ test('Should handle the sort filter', ()=> {
+  const handleFilter = jest.fn()
+  render(<FilterSearch handleFilter={handleFilter} />);
+  //@ts-expect-error
+  expect(screen.getByRole('option', {name: 'name'}).selected).toBeTruthy()
+  userEvent.selectOptions(
+    screen.getByRole('combobox'),
+    screen.getByRole('option', {name: 'type'})
+    );
+  //@ts-expect-error
+  expect(screen.getByRole('option', {name: 'type'}).selected).toBeTruthy()
+ })
+
+ test('should render the data', ()=> {
+  render(<FilterData data={jsonData}/>)
+  expect(screen.getByText(/Public Holiday policy.pdf : 2016-12-06/)).toBeInTheDocument();
+  expect(screen.getByText(/Employee Handbook.pdf : 2017-01-06/)).toBeInTheDocument();
+  expect(screen.getByText(/Cost centres.csv : 2016-08-12/)).toBeInTheDocument();
+})
+
+test('should render the data within the Folder', ()=> {
+  const mockDataForFolders = [{     
+    "type": "folder",     
+    "name": "Expenses",     
+    "files": 
+    [
+        {         
+            "type": "doc",         
+            "name": "Expenses claim form",
+            "added": "2017-05-02"
+        },
+        {         
+            "type": "doc",
+            "name": "Fuel allowances",
+            "added": "2017-05-03"
+        }
+    ]
+},    {     
+  "type": "folder",     
+  "name": "Misc",     
+  "files": 
+  [
+      {         
+          "type": "doc",         
+          "name": "Christmas party",         
+          "added": "2017-12-01"
+      },
+      {         
+          "type": "mov",         
+          "name": "Welcome to the company!",         
+          "added": "2015-04-24"
+      }
+  ]
+}]
+  render(<Folder items={mockDataForFolders[0]}/>)
+  fireEvent.click(screen.getByRole('button', {name: '📁 Expenses'}))
+
+  expect(screen.getByText(/Expenses claim form.doc : 2017-05-02/)).toBeInTheDocument();
+  expect(screen.getByText(/Fuel allowances.doc : 2017-05-03/)).toBeInTheDocument();
+
+  render(<Folder items={mockDataForFolders[1]}/>)
+  fireEvent.click(screen.getByRole('button', {name: '📁 Misc'}))
+  expect(screen.getByText(/Christmas party.doc : 2017-12-01/)).toBeInTheDocument();
+  expect(screen.getByText(/Welcome to the company!.mov : 2015-04-24/)).toBeInTheDocument();
+})
+test('should handle the filterOptions fn', () => {
+  /**
+   * There is something odd happening with this function, given the time constraint
+   * I haven't had the opportunity to look into it.
+   */
+  expect(filterOptions(jsonData,'name')[0].name).toEqual("Cost centres")
+
+  expect(filterOptions(jsonData,'type')[1].name).toEqual("Misc")
+  expect(filterOptions(jsonData,'type')[3].name).toEqual("Employee Handbook")
+
+  expect(filterOptions(jsonData,'added')[2].name).toEqual("Misc")
+  expect(filterOptions(jsonData,'added')[4].name).toEqual("Employee Handbook")
+
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { jsonData } from './JSONData';
 
-function App() {
+/**
+ * 
+ * @param param0 JSON objects with type 'folder'
+ * @returns JSX in folder form with a collapsing list of contents
+ */
+export const Folder = ({items}:any) => {
+  const [contents,showContents] = React.useState(false)
+  return <>
+  <button onClick={()=>showContents(!contents)}>{'📁 '+items.name}</button>
+  {contents ? items.files.map((item :any)=> (
+    <ul className='Items' key={item.name}>{item.name+'.'+item.type+' : '+item.added}</ul>
+  )) : null}
+  </>
+}
+/**
+ * 
+ * @param param0 JSON object
+ * @returns JSX elements in a list of items or Folders
+ */
+export const FilterData = ({data}:any) => {
+  return <>
+  {data.map((data:any)=> data.type!=='folder' ? 
+  <div className='Items' key={data.name}>{data.name+'.'+data.type+' : '+data.added}</div> :
+  <Folder items={data} key={data.name}/>)}
+  </>
+}
+
+/**
+ * 
+ * @param param0 a callback filter to set the filter value in parent
+ * @returns JSX for the input and sort elements
+ */
+export const FilterSearch = ({handleFilter}:any) => {
+  const [value,setValue] = React.useState()
+  const handleChange = (event:any) => {
+    setValue(event.target.value)
+    handleFilter(value)
+  };
+return (
+  <div className='filter'>
+  <input placeholder='Search...'></input>
+  Sort
+  <select test-id='select' value={value} onChange={handleChange}>
+    <option value='name'>name</option>
+    <option value='type'>type</option>
+    <option value='date'>date</option>
+  </select>
+  </div>
+)
+}
+/**
+ * 
+ * @param data JSON object
+ * @param filter used to return sorted JSON object
+ * @returns 
+ */
+export const filterOptions = (data:any, filter:string) => {
+
+  if ( filter === 'name') {
+    data.sort((a:any,b:any)  => (a.name > b.name) ? 1 : -1)
+  } else if ( filter === 'type') {
+    data.sort((a:any,b:any)  => (a.type > b.type) ? 1 : -1)
+  } else if ( filter === 'added') {
+    data.sort((a:any,b:any)  => a.added && b.added ? ( a.added > b.added) ? 1 : -1 : 0)
+  }
+  return data
+}
+/**
+ * 
+ * @returns Main Component
+ */
+
+export const App =() => {
+  const [data,setData] = React.useState(jsonData)
+  const [filter,setFilter] = React.useState('')
+  const handleFilter = (value:string) => setFilter(value)
+  
+  React.useEffect(()=> {
+    setData(filterOptions(data,filter))
+  },[data,filter])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      Documents
+    <div className='Main'>
+      <FilterSearch handleFilter={handleFilter} />
+      <FilterData data={data} />
+    </div>
     </div>
   );
 }
-
-export default App;
